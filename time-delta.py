4bd81fdf2492e35c5b666d63b9b96d2cedaf300c
Marco Zuñiga <marco.zuniga@autoweb.com>
2020-03-21 11:45:40 -0600

    Time Delta Challenge

---
+++ time-delta.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import timedelta, datetime
+
+FORMAT = '%a %d %b %Y %H:%M:%S %z'
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    d1 = datetime.strptime(t1, FORMAT)
+    d2 = datetime.strptime(t2, FORMAT)
+    res = d1 - d2
+    return str(abs(int(res.total_seconds())))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
